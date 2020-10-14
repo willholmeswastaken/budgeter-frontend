b817d24710bfd9393e3e796e7a879bb9c5c2d2b7
willholmeswastaken <willholmes97@icloud.com>
2020-10-14 21:45:29 +0100

    Auth working - next to decide on how im gonna make this thing look

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
-function App() {
+const App = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
+      { isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Auth0Provider} from "@auth0/auth0-react";
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import config from "./auth_config.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={config.domain}
+      clientId={config.clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
