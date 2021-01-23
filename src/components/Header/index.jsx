f052823a0c6df202490544f63e8eaf3ae352f446
willholmeswastaken <willholmes97@icloud.com>
2021-01-23 18:56:33 +0000

    Fixed prettier rulings and integrated with eslint

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Button, Heading } from 'grommet';
 import { Notification } from 'grommet-icons';
-import React from 'react';
 
-export default ({ toggleSidebar }) => (
-    <Box
-        tag='header'
-        direction='row'
-        align='center'
-        justify='between'
-        background='brand'
-        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-        elevation='medium'
-        style={{ zIndex: '1' }}
-    >
-        <Heading
-            level='3'
-            margin='none'>
-            Budgeter
-        </Heading>
-        <Button
-            icon={<Notification />}
-            onClick={toggleSidebar}
-        />
-    </Box>
-)
+const Header = ({ toggleSidebar }) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation="medium"
+    style={{ zIndex: '1' }}
+  >
+    <Heading level="3" margin="none">
+      Budgeter
+    </Heading>
+    <Button icon={<Notification />} onClick={toggleSidebar} />
+  </Box>
+);
+
+Header.propTypes = {
+  toggleSidebar: PropTypes.func.isRequired,
+};
+
+export default Header;
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,43 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Button, Collapsible, Layer } from 'grommet';
 import { FormClose } from 'grommet-icons';
-import React from 'react';
 
-export default ({ showSidebar, isSmallDisplay, setShowSidebar }) => (
-    (!showSidebar || !isSmallDisplay)
-        ? (
-            <Collapsible direction="horizontal" open={showSidebar}>
-                <Box
-                    width='medium'
-                    background='light-2'
-                    elevation='small'
-                    align='center'
-                    justify='center'
-                >
-                    sidebar
-                </Box>
-            </Collapsible>
-        )
-        : (
-            <Layer>
-                <Box
-                    background='light-2'
-                    tag='header'
-                    justify='end'
-                    align='center'
-                    direction='row'
-                >
-                    <Button
-                        icon={<FormClose />}
-                        onClick={() => setShowSidebar(false)}
-                    />
-                </Box>
-                <Box
-                    fill
-                    background='light-2'
-                    align='center'
-                    justify='center'>
-                    sidebar
-                </Box>
-            </Layer>
-        )
-);
+const Sidebar = ({ showSidebar, isSmallDisplay, setShowSidebar }) =>
+  !showSidebar || !isSmallDisplay ? (
+    <Collapsible direction="horizontal" open={showSidebar}>
+      <Box width="medium" background="light-2" elevation="small" align="center" justify="center">
+        sidebar
+      </Box>
+    </Collapsible>
+  ) : (
+    <Layer>
+      <Box background="light-2" tag="header" justify="end" align="center" direction="row">
+        <Button icon={<FormClose />} onClick={() => setShowSidebar(false)} />
+      </Box>
+      <Box fill background="light-2" align="center" justify="center">
+        sidebar
+      </Box>
+    </Layer>
+  );
+
+Sidebar.propTypes = {
+  showSidebar: PropTypes.bool,
+  isSmallDisplay: PropTypes.bool.isRequired,
+  setShowSidebar: PropTypes.func.isRequired,
+};
+
+Sidebar.defaultProps = {
+  showSidebar: false,
+};
+
+export default Sidebar;
