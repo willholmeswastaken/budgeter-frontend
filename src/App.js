7911211f30ebe54098617f56601024203dffccc6
willholmeswastaken <willholmes97@icloud.com>
2021-01-26 13:11:09 +0000

    Tailwind migration started...

--- src/App.js
+++ src/App.js
@@ -3,34 +3,24 @@ import { Box, Grommet } from 'grommet';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import theme from './theme';
-
-import Header from './components/Header';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 const App = () => (
   <Grommet theme={theme}>
     <Box>
-      <Header />
-      <Box
-        background="light-1"
-        fill
-        direction="row"
-        flex
-        overflow={{ horizontal: 'hidden', vertical: 'hidden' }}
-        height={{ min: '100vh' }}
-      >
-        <Box flex align="top" justify="top">
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={Landing} />
-              <Route path="/home" component={Home} />
-              <Route component={NotFound} />
-            </Switch>
-          </BrowserRouter>
-        </Box>
-      </Box>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </Box>
   </Grommet>
 );
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // this will remove any unused styles for performance
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
