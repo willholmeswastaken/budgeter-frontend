42a8ba02b5a16757cc4e08c401544cedb5ec53b3
willholmeswastaken <willholmes97@icloud.com>
2021-03-10 21:59:59 +0000

    Kinda gone for an instagram look... i like it

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -4,8 +4,38 @@ import Emoji from "../components/Emoji";
 const Overview = () => (
   <div className="pt-10">
     <div className="flex flex-col relative px-4 mx-auto max-w-xl ">
-      {" "}
-      <div className="relative justify-center bg-white shadow-lg rounded-lg py-2 hvr-grow cursor-pointer">
+      <ul className="flex space-x-6 bg-white rounded-lg shadow-sm">
+        <li className="flex flex-col items-center space-y-1 my-2 ml-2">
+          <div className="relative bg-gradient-to-tr from-green-light to-green-dark p-1 rounded-full cursor-pointer">
+            <a className="block bg-white p-1 rounded-full h-20 w-20 transform transition hover:-rotate-6">
+              <Image
+                src="/budgeter-logo.png"
+                alt="Budgeter logo"
+                height={100}
+                width={100}
+                className="object-contain md:object-scale-down"
+              />
+            </a>
+            <button className="absolute bottom-70 right-0 bg-green h-8 w-8 rounded-full text-white text-2xl font-mono border-4 border-white flex justify-center items-center">+</button>
+          </div>
+          <a>New</a>
+        </li>
+        <li className="flex flex-col items-center space-y-1 my-2 ml-1">
+          <div className="bg-gradient-to-tr from-green-light to-green-dark p-1 rounded-full cursor-pointer">
+            <a className="block bg-white p-1 rounded-full h-20 w-20 transform transition hover:-rotate-6">
+              <Image
+                src="/budgeter-logo.png"
+                alt="Budgeter logo"
+                height={100}
+                width={100}
+                className="object-contain md:object-scale-down"
+              />
+            </a>
+          </div>
+          <a className="font-semibold">Monzo</a>
+        </li>
+      </ul>{" "}
+      <div className="relative mt-5 justify-center bg-white shadow-lg rounded-lg py-2 hvr-float cursor-pointer">
         <div className="flex">
           <div className="px-2">
             <Image
