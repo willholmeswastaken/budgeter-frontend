bd5a2b87e8dd7eaca2ea12a05b5b6dccfb2b896d
willholmeswastaken <willholmes97@icloud.com>
2021-03-11 18:51:56 +0000

    added floating action button... cool

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -4,6 +4,21 @@ import Emoji from "../components/Emoji";
 
 const Overview = () => (
   <div className="pt-10">
+    <button className="absolute right-10 bottom-10 text-white px-4 w-auto h-12 bg-green rounded-full hover:bg-green-dark active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+      <svg
+        viewBox="0 0 20 20"
+        enable-background="new 0 0 20 20"
+        className="w-6 h-6 inline-block mr-1 mb-1"
+      >
+        <path
+          fill="#FFFFFF"
+          d="M16,10c0,0.553-0.048,1-0.601,1H11v4.399C11,15.951,10.553,16,10,16c-0.553,0-1-0.049-1-0.601V11H4.601
+                                    C4.049,11,4,10.553,4,10c0-0.553,0.049-1,0.601-1H9V4.601C9,4.048,9.447,4,10,4c0.553,0,1,0.048,1,0.601V9h4.399
+                                    C15.952,9,16,9.447,16,10z"
+        />
+      </svg>
+      <span className="pt-1">Update Balances</span>
+    </button>
     <div className="flex flex-col relative px-4 mx-auto max-w-xl ">
       <ul className="flex space-x-6 bg-white rounded-lg shadow-sm">
         <li className="flex flex-col items-center space-y-1 my-2 ml-2">
